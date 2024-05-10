6712cc965fc0b30dfd838ccc37eb7c62158657f9
Giorgi Gurgenindze <giorgigurgenidze133@gmail.com>
2024-05-10 19:29:15 +0400

    removed cors and body parser

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 // const morgan = require("morgan");
-const bodyParser = require("body-parser");
-const cors = require("cors");
+// const bodyParser = require("body-parser");
+// const cors = require("cors");
 const feedbackRouter = require("./routes/feedbacksRouter");
 
 const app = express();
@@ -9,8 +9,6 @@ const app = express();
 // if (process.env.NODE_ENV === "development") {
 //   app.use(morgan("dev"));
 // }
-app.use(cors());
-app.use(bodyParser.json());
 app.use(express.json());
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
